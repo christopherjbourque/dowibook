8ab4e34bcbbbfa71069bee7e5c972d7a1576e505
christopherjbourque <c@bourque.xyz>
2020-10-21 00:00:38 -0400

    Create preliminary routes for testing.

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ const app = express();
 // Connect MongoDB database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
 app.get("/", (req, res) => res.send(`The API is running`));
+
+
+// Define routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
 
 const PORT = process.env.PORT || 8000;
 
