cf9da4d20d8567f6e67508e6724ca0c2a29fb875
mralimov <mralimov89@gmail.com>
2020-10-22 18:56:34 -0400

    Register.js update

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -10,13 +10,21 @@ const Register = () => {
   const { name, email, password, password2 } = formData;
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Password do not match');
+    } else {
+      console.log(formData);
+    }
+  };
   return (
     <Fragment>
       <h1 className='large text-primary'>Sign Up</h1>
       <p className='lead'>
         <i className='fas fa-user'></i> Create Your Account
       </p>
-      <form className='form' action='create-profile.html'>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
         <div className='form-group'>
           <input
             type='text'
@@ -28,7 +36,14 @@ const Register = () => {
           />
         </div>
         <div className='form-group'>
-          <input type='email' placeholder='Email Address' name='email' />
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
           <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
@@ -39,6 +54,9 @@ const Register = () => {
             type='password'
             placeholder='Password'
             name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
             minLength='6'
           />
         </div>
@@ -47,6 +65,9 @@ const Register = () => {
             type='password'
             placeholder='Confirm Password'
             name='password2'
+            value={password2}
+            onChange={(e) => onChange(e)}
+            required
             minLength='6'
           />
         </div>
