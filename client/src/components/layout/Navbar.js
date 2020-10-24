69e6bfb4d060e04de44be4823b308cbe24bd8092
mralimov <mralimov89@gmail.com>
2020-10-24 12:26:28 -0400

    update

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
-const Navbar = ({ auth: { isAuthenticated }, logout }) => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
@@ -48,7 +48,9 @@ const Navbar = ({ auth: { isAuthenticated }, logout }) => {
           <i className='fas fa-code'></i> DevConnector
         </Link>
       </h1>
-      <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
