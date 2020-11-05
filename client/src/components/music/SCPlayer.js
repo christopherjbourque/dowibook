c589c25e4b83023ce54bdc38c86d5204a660c4d7
wilsonbirch <wilsonbirch@gmail.com>
2020-11-04 20:27:59 -0500

    added form input and tracklist buttons

--- client/src/components/music/SCPlayer.js
+++ client/src/components/music/SCPlayer.js
@@ -7,15 +7,36 @@ const SCPlayer = ({ login, isAuthenticated }) => {
 	return (
 		<div className='scPlayer'>
 			{isAuthenticated && (
-				<iframe
-					title='scplayer'
-					width='100%'
-					height='166'
-					scrolling='no'
-					frameborder='no'
-					allow='autoplay'
-					src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/752572219&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
-				></iframe>
+				<section>
+					<iframe
+						title='scplayer'
+						width='100%'
+						height='166'
+						scrolling='no'
+						frameborder='no'
+						allow='autoplay'
+						src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/752572219&color=%23ff5500&auto_play=true&hide_related=false&show_teaser=true&visual=true'
+					></iframe>
+					<form className='scInputForm'>
+						<div>
+							<input
+								id='scInput'
+								class='scInput'
+								type='text'
+								placeholder='soundcloud embed link here'
+							/>
+						</div>
+					</form>
+					<button className='btn btn-light'>
+						<i type='submit' class='fas fa-play text-primary'></i> Submit
+					</button>
+					<button className='btn btn-light'>
+						<i className='fas fa-ice-cream text-primary'></i> Chill
+					</button>
+					<button className='btn btn-light'>
+						<i className='fas fa-guitar text-primary'></i> Rock
+					</button>
+				</section>
 			)}
 		</div>
 	);
